652b70449134a814661febc81779770c4a217976
Matt Dembiczak <matt@dembiczak.net>
2025-04-16 21:56:12 -0400

    add private school assignment tasks

--- fred_pop_gen/task_assign_schools.py
+++ fred_pop_gen/task_assign_schools.py
@@ -65,26 +65,25 @@ for _county in get_county_fips():
             DATA_CATALOG[f"households_{_county}"],
         ],
         sch_df: Annotated[pd.DataFrame, DATA_CATALOG[f"public_schools_{_county}"]],
-    ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"pubsch_hh_distance_{_county}"]]:
+    ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"public_hh_distance_{_county}"]]:
         hh_df = filter_households_by_resident_enrollment(p_df, hh_df, Enrollment.PUBLIC)
+        return get_school_household_distances_in_county(hh_df, sch_df)
 
-        hh_idx, sch_idx = np.meshgrid(
-            hh_df.index.to_numpy(), sch_df.index.to_numpy(), indexing="ij"
+    @task(id=_county)
+    def get_private_school_household_distances_in_county(
+        p_df: Annotated[
+            pd.DataFrame, DATA_CATALOG[f"persons_w_grade_enrollment_{_county}"]
+        ],
+        hh_df: Annotated[
+            pd.DataFrame,
+            DATA_CATALOG[f"households_{_county}"],
+        ],
+        sch_df: Annotated[pd.DataFrame, DATA_CATALOG[f"private_schools_{_county}"]],
+    ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"private_hh_distance_{_county}"]]:
+        hh_df = filter_households_by_resident_enrollment(
+            p_df, hh_df, Enrollment.PRIVATE
         )
-        hh_idx = hh_idx.ravel()
-        sch_idx = sch_idx.ravel()
-
-        hh_lat = hh_df.loc[hh_idx, "lat"].to_numpy()
-        hh_lon = hh_df.loc[hh_idx, "lon"].to_numpy()
-        sch_lat = sch_df.loc[sch_idx, "lat"].to_numpy()
-        sch_lon = sch_df.loc[sch_idx, "lon"].to_numpy()
-
-        distances = haversine(hh_lat, hh_lon, sch_lat, sch_lon)
-
-        df = pd.DataFrame({"hh_id": hh_idx, "sch_id": sch_idx, "distance": distances})
-        df = df.sort_values(by="distance", ascending=True)
-
-        return df
+        return get_school_household_distances_in_county(hh_df, sch_df)
 
     @task(id=_county)
     def assign_public_schools_in_county(
@@ -92,82 +91,23 @@ for _county in get_county_fips():
             pd.DataFrame, DATA_CATALOG[f"persons_w_grade_enrollment_{_county}"]
         ],
         sch_df: Annotated[pd.DataFrame, DATA_CATALOG[f"public_schools_{_county}"]],
-        dist_df: Annotated[pd.DataFrame, DATA_CATALOG[f"pubsch_hh_distance_{_county}"]],
-    ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_school_{_county}"]]:
+        dist_df: Annotated[pd.DataFrame, DATA_CATALOG[f"public_hh_distance_{_county}"]],
+    ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_public_school_{_county}"]]:
         p_df = p_df.loc[p_df["enrollment"] == Enrollment.PUBLIC]
-        p_df["school_id"] = None
+        return assign_schools_to_persons(p_df, sch_df, dist_df)
 
-        hh_person_groups = p_df.groupby("hh_id")
-
-        # track current number of assigned students per school
-        enrollment = {sch_id: 0 for sch_id in sch_df.index.to_list()}
-
-        # track assignments to apply at end of loop
-        assignments = {}
-
-        # precompile eligible grades per school
-        eligible_grades = {
-            sch_id: Grade.range(Grade(sch["lowest_grade"]), Grade(sch["highest_grade"]))
-            for sch_id, sch in sch_df.iterrows()
-        }
-
-        for edge in dist_df.itertuples():
-            # cast to Any to avoid static typing issues with accessing
-            # NamedTuple fields
-            edge: Any = cast(Any, edge)
-
-            sch = sch_df.loc[edge.sch_id]
-
-            capacity = sch["enrollment_total"]
-            if enrollment[edge.sch_id] > capacity:
-                continue
-
-            # get persons in current household
-            persons_to_assign = pd.DataFrame(
-                hh_person_groups.get_group(edge.hh_id)
-            ).index
-
-            # filter by children who have not yet been assigned
-            persons_to_assign = persons_to_assign[
-                persons_to_assign.map(lambda x: x not in assignments)
-            ]
-
-            # filter by children who are eligible for the school
-            persons_to_assign = persons_to_assign[
-                persons_to_assign.map(
-                    lambda x: p_df.loc[x, "grade"] in eligible_grades[edge.sch_id]
-                )
-            ]
-
-            # assign
-            assignments.update({p_id: edge.sch_id for p_id in persons_to_assign})
-            enrollment[edge.sch_id] += len(persons_to_assign)
-
-            # check for early exit
-            n_unassigned = len(p_df) - len(assignments)
-            if n_unassigned == 0:
-                break
-
-        # assign leftover students to nearest school
-        def assign_person_to_nearest_school(p_id):
-            hh_id = p_df.loc[p_id, "hh_id"]
-            for edge in dist_df.loc[dist_df["hh_id"] == hh_id].itertuples():
-                if p_df.loc[p_id, "grade"] not in eligible_grades[edge.sch_id]:
-                    continue
-
-                assignments.update({p_id: edge.sch_id})
-                enrollment[edge.sch_id] += 1
-
-        unassigned_df = p_df[~p_df.index.isin(assignments)]
-        unassigned_df.index.map(assign_person_to_nearest_school)
-
-        p_df["school_id"] = p_df.index.map(assignments.get)
-
-        # TODO: handle case where there are no schools that offer PREK in county
-        unassigned_df = p_df[p_df["school_id"].isnull()]
-        assert unassigned_df.empty
-
-        return p_df
+    @task(id=_county)
+    def assign_private_schools_in_county(
+        p_df: Annotated[
+            pd.DataFrame, DATA_CATALOG[f"persons_w_grade_enrollment_{_county}"]
+        ],
+        sch_df: Annotated[pd.DataFrame, DATA_CATALOG[f"private_schools_{_county}"]],
+        dist_df: Annotated[
+            pd.DataFrame, DATA_CATALOG[f"private_hh_distance_{_county}"]
+        ],
+    ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_private_school_{_county}"]]:
+        p_df = p_df.loc[p_df["enrollment"] == Enrollment.PRIVATE]
+        return assign_schools_to_persons(p_df, sch_df, dist_df)
 
 
 def map_age_to_grade(age: int) -> Grade | None:
@@ -204,3 +144,101 @@ def map_age_to_grade(age: int) -> Grade | None:
             return Grade.TWELFTH
         case _:
             return None
+
+
+def get_school_household_distances_in_county(
+    hh_df: pd.DataFrame, sch_df: pd.DataFrame
+) -> pd.DataFrame:
+    hh_idx, sch_idx = np.meshgrid(
+        hh_df.index.to_numpy(), sch_df.index.to_numpy(), indexing="ij"
+    )
+    hh_idx = hh_idx.ravel()
+    sch_idx = sch_idx.ravel()
+
+    hh_lat = hh_df.loc[hh_idx, "lat"].to_numpy()
+    hh_lon = hh_df.loc[hh_idx, "lon"].to_numpy()
+    sch_lat = sch_df.loc[sch_idx, "lat"].to_numpy()
+    sch_lon = sch_df.loc[sch_idx, "lon"].to_numpy()
+
+    distances = haversine(hh_lat, hh_lon, sch_lat, sch_lon)
+
+    df = pd.DataFrame({"hh_id": hh_idx, "sch_id": sch_idx, "distance": distances})
+    df = df.sort_values(by="distance", ascending=True)
+
+    return df
+
+
+def assign_schools_to_persons(
+    p_df: pd.DataFrame, sch_df: pd.DataFrame, dist_df: pd.DataFrame
+) -> pd.DataFrame:
+    p_df["school_id"] = None
+
+    hh_person_groups = p_df.groupby("hh_id")
+
+    # track current number of assigned students per school
+    enrollment = {sch_id: 0 for sch_id in sch_df.index.to_list()}
+
+    # track assignments to apply at end of loop
+    assignments = {}
+
+    # precompile eligible grades per school
+    eligible_grades = {
+        sch_id: Grade.range(Grade(sch["lowest_grade"]), Grade(sch["highest_grade"]))
+        for sch_id, sch in sch_df.iterrows()
+    }
+
+    for edge in dist_df.itertuples():
+        # cast to Any to avoid static typing issues with accessing
+        # NamedTuple fields
+        edge: Any = cast(Any, edge)
+
+        sch = sch_df.loc[edge.sch_id]
+
+        capacity = sch["enrollment_total"]
+        if enrollment[edge.sch_id] > capacity:
+            continue
+
+        # get persons in current household
+        persons_to_assign = pd.DataFrame(hh_person_groups.get_group(edge.hh_id)).index
+
+        # filter by children who have not yet been assigned
+        persons_to_assign = persons_to_assign[
+            persons_to_assign.map(lambda x: x not in assignments)
+        ]
+
+        # filter by children who are eligible for the school
+        persons_to_assign = persons_to_assign[
+            persons_to_assign.map(
+                lambda x: p_df.loc[x, "grade"] in eligible_grades[edge.sch_id]
+            )
+        ]
+
+        # assign
+        assignments.update({p_id: edge.sch_id for p_id in persons_to_assign})
+        enrollment[edge.sch_id] += len(persons_to_assign)
+
+        # check for early exit
+        n_unassigned = len(p_df) - len(assignments)
+        if n_unassigned == 0:
+            break
+
+    # assign leftover students to nearest school
+    def assign_person_to_nearest_school(p_id):
+        hh_id = p_df.loc[p_id, "hh_id"]
+        for edge in dist_df.loc[dist_df["hh_id"] == hh_id].itertuples():
+            if p_df.loc[p_id, "grade"] not in eligible_grades[edge.sch_id]:
+                continue
+
+            assignments.update({p_id: edge.sch_id})
+            enrollment[edge.sch_id] += 1
+
+    unassigned_df = p_df[~p_df.index.isin(assignments)]
+    unassigned_df.index.map(assign_person_to_nearest_school)
+
+    p_df["school_id"] = p_df.index.map(assignments.get)
+
+    # TODO: handle case where there are no schools that offer PREK in county
+    unassigned_df = p_df[p_df["school_id"].isnull()]
+    assert unassigned_df.empty
+
+    return p_df
