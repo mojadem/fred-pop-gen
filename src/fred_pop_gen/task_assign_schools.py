b6447d771cf30618c8d79be6102a16c8dfb467cc
Matt Dembiczak <matt@dembiczak.net>
2025-05-30 15:37:59 -0400

    refactor school assignment

--- src/fred_pop_gen/task_assign_schools.py
+++ src/fred_pop_gen/task_assign_schools.py
@@ -10,7 +10,6 @@ from fred_pop_gen.config import (
 )
 from fred_pop_gen.constants import Enrollment, Grade
 from fred_pop_gen.utils import (
-    filter_households_by_resident_enrollment,
     get_county_fips,
     haversine,
 )
@@ -19,80 +18,76 @@ from fred_pop_gen.utils import (
 for _county in get_county_fips():
 
     @task(id=_county)
-    def task_get_public_school_household_distances_in_county(
+    def task_get_persons_for_public_school_assignment_in_county(
+        county: Annotated[str, _county],
         p_df: Annotated[
             pd.DataFrame, DATA_CATALOG[f"persons_w_enrollment_{STATE_FIPS}"]
         ],
-        hh_df: Annotated[
-            pd.DataFrame,
-            DATA_CATALOG[f"households_{_county}"],
+    ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_pub_enrollment_{_county}"]]:
+        p_df = p_df.loc[p_df["county_fips"] == county]
+        p_df = p_df.loc[p_df["enrollment"] == Enrollment.PUBLIC]
+
+        return p_df
+
+    @task(id=_county)
+    def task_get_public_school_distances_in_county(
+        p_df: Annotated[
+            pd.DataFrame, DATA_CATALOG[f"persons_w_pub_enrollment_{_county}"]
         ],
         sch_df: Annotated[pd.DataFrame, DATA_CATALOG[f"public_schools_{_county}"]],
     ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"public_hh_distance_{_county}"]]:
-        p_df = p_df.loc[p_df["enrollment"] == Enrollment.PUBLIC]
-        p_hh_df = merge_p_hh_df(p_df, hh_df)
-
-        return get_school_distances(p_hh_df, sch_df)
+        return get_school_distances(p_df, sch_df)
 
     @task(id=_county)
     def task_assign_public_schools_in_county(
+        county: Annotated[str, _county],
         p_df: Annotated[
-            pd.DataFrame, DATA_CATALOG[f"persons_w_enrollment_{STATE_FIPS}"]
+            pd.DataFrame, DATA_CATALOG[f"persons_w_pub_enrollment_{_county}"]
         ],
         sch_df: Annotated[pd.DataFrame, DATA_CATALOG[f"public_schools_{_county}"]],
         dist_df: Annotated[pd.DataFrame, DATA_CATALOG[f"public_hh_distance_{_county}"]],
     ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_public_school_{_county}"]]:
-        p_df = p_df.loc[p_df["enrollment"] == Enrollment.PUBLIC]
-
         return assign_schools_to_persons(p_df, sch_df, dist_df)
 
 
-def task_get_private_school_distances(
+def task_get_persons_for_private_school_assignment(
     p_df: Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_enrollment_{STATE_FIPS}"]],
-    hh_df: Annotated[
-        pd.DataFrame,
-        DATA_CATALOG[f"households_{STATE_FIPS}"],
+) -> Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_priv_enrollment_{STATE_FIPS}"]]:
+    p_df = p_df.loc[p_df["enrollment"] == Enrollment.PRIVATE]
+
+    return p_df
+
+
+def task_get_private_school_distances(
+    p_df: Annotated[
+        pd.DataFrame, DATA_CATALOG[f"persons_w_priv_enrollment_{STATE_FIPS}"]
     ],
     sch_df: Annotated[pd.DataFrame, DATA_CATALOG[f"private_schools_{STATE_FIPS}"]],
 ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"private_school_distances_{STATE_FIPS}"]]:
-    p_df = p_df.loc[p_df["enrollment"] == Enrollment.PRIVATE]
-    p_hh_df = merge_p_hh_df(p_df, hh_df)
-
-    return get_school_distances(p_hh_df, sch_df)
+    return get_school_distances(p_df, sch_df)
 
 
 def task_assign_private_schools(
-    p_df: Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_enrollment_{STATE_FIPS}"]],
+    p_df: Annotated[
+        pd.DataFrame, DATA_CATALOG[f"persons_w_priv_enrollment_{STATE_FIPS}"]
+    ],
     sch_df: Annotated[pd.DataFrame, DATA_CATALOG[f"private_schools_{STATE_FIPS}"]],
     dist_df: Annotated[
         pd.DataFrame, DATA_CATALOG[f"private_school_distances_{STATE_FIPS}"]
     ],
 ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_private_school_{STATE_FIPS}"]]:
-    p_df = p_df.loc[p_df["enrollment"] == Enrollment.PRIVATE]
-
     return assign_schools_to_persons(p_df, sch_df, dist_df)
 
 
-def merge_p_hh_df(p_df: pd.DataFrame, hh_df: pd.DataFrame) -> pd.DataFrame:
-    p_hh_df = p_df.merge(hh_df, on="hh_id", how="left")
-
-    # we must retain the orignal person index as they function as each person's
-    # unique id
-    assert len(p_df) == len(p_hh_df)
-    p_hh_df = p_hh_df.set_index(p_df.index)
-
-    return p_hh_df
-
-
-def get_school_distances(p_hh_df: pd.DataFrame, sch_df: pd.DataFrame) -> pd.DataFrame:
+def get_school_distances(p_df: pd.DataFrame, sch_df: pd.DataFrame) -> pd.DataFrame:
     p_idx, sch_idx = np.meshgrid(
-        p_hh_df.index.to_numpy(), sch_df.index.to_numpy(), indexing="ij"
+        p_df.index.to_numpy(), sch_df.index.to_numpy(), indexing="ij"
     )
     p_idx = p_idx.ravel()
     sch_idx = sch_idx.ravel()
 
-    p_lat = p_hh_df.loc[p_idx, "lat"].to_numpy()
-    p_lon = p_hh_df.loc[p_idx, "lon"].to_numpy()
+    p_lat = p_df.loc[p_idx, "lat"].to_numpy()
+    p_lon = p_df.loc[p_idx, "lon"].to_numpy()
     sch_lat = sch_df.loc[sch_idx, "lat"].to_numpy()
     sch_lon = sch_df.loc[sch_idx, "lon"].to_numpy()
 
@@ -114,8 +109,6 @@ def assign_schools_to_persons(
 
     # track assignments to apply at end of loop
     assignments = {}
-
-    n = 0
 
     # precompile eligible grades per school
     eligible_grades = {
@@ -144,7 +137,6 @@ def assign_schools_to_persons(
         # assign
         assignments.update({edge.p_id: edge.sch_id})
         enrollment[edge.sch_id] += 1
-        n += 1
 
         # check for early exit
         n_unassigned = len(p_df) - len(assignments)
@@ -155,15 +147,17 @@ def assign_schools_to_persons(
     # enrollment is evenly distributed
     capacity_scale_factor = len(p_df) / sch_df["enrollment_total"].sum()
     capacity_scale_factor += 0.1  # add some headroom
+    if capacity_scale_factor < 1:
+        capacity_scale_factor = 1
 
     # assign leftover students to nearest school
     def assign_person_to_nearest_school(p_id):
         for edge in dist_df.loc[dist_df["p_id"] == p_id].itertuples():
-            capacity = (
-                sch_df.loc[edge.sch_id, "enrollment_total"] * capacity_scale_factor
-            )
-            if enrollment[edge.sch_id] > capacity:
-                continue
+            # capacity = (
+            #     sch_df.loc[edge.sch_id, "enrollment_total"] * capacity_scale_factor
+            # )
+            # if enrollment[edge.sch_id] > capacity:
+            #     continue
 
             if p_df.loc[p_id, "grade"] not in eligible_grades[edge.sch_id]:
                 continue
@@ -177,8 +171,23 @@ def assign_schools_to_persons(
 
     p_df["school_id"] = p_df.index.map(assignments.get)
 
-    # TODO: handle case where there are no schools that offer PREK in county
-    unassigned_df = p_df[p_df["school_id"].isnull()]
-    assert unassigned_df.empty
+    # TODO: handle case where there are no schools that offer PREK in county,
+    # for now, we will leave them unassigned, as the numbers aren't too large
+    # and it is unlikely there would be PREK kids attending out of county
+    # schools
+    #
+    # TODO: handle case where there are a large amount of private school
+    # enrolees are leftover, in some states (such as WY), there is much less
+    # private school capacity available then private school enrollees
+
+    # unassigned_df = p_df[p_df["school_id"].isnull()]
+
+    # some helpful debug statements:
+    #
+    # print(capacity_scale_factor)
+    # sch_df["enrollment"] = sch_df.index.map(lambda x: enrollment[x])
+    # print(sch_df[["lowest_grade", "highest_grade", "enrollment", "enrollment_total"]])
+    # print(unassigned_df[["grade"]])
+    # assert unassigned_df.empty
 
     return p_df
