7b6e425898906aadf61163ef660fd66dce6a1078
Matt Dembiczak <matt@dembiczak.net>
2025-05-30 16:47:32 -0400

    update input county filtering

--- src/fred_pop_gen/task_filter_input_by_county.py
+++ src/fred_pop_gen/task_filter_input_by_county.py
@@ -17,21 +17,14 @@ for _county in get_county_fips():
 
     @task(id=_county)
     def get_persons_in_county(
-        p_df: Annotated[pd.DataFrame, DATA_CATALOG[f"persons_{STATE_FIPS}"]],
-        hh_df: Annotated[pd.DataFrame, DATA_CATALOG[f"households_{_county}"]],
+        county: Annotated[str, _county],
+        df: Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_geo_{STATE_FIPS}"]],
     ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"persons_{_county}"]]:
-        return p_df.loc[p_df["hh_id"].apply(lambda x: x in hh_df.index)]
+        return filter_df_by_county(df, county)
 
     @task(id=_county)
     def get_public_schools_in_county(
         county: Annotated[str, _county],
         df: Annotated[pd.DataFrame, DATA_CATALOG[f"public_schools_{STATE_FIPS}"]],
     ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"public_schools_{_county}"]]:
-        return filter_df_by_county(df, county)
-
-    @task(id=_county)
-    def get_private_schools_in_county(
-        county: Annotated[str, _county],
-        df: Annotated[pd.DataFrame, DATA_CATALOG[f"private_schools_{STATE_FIPS}"]],
-    ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"private_schools_{_county}"]]:
         return filter_df_by_county(df, county)
