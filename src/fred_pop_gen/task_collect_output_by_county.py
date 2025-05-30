1b02575f38fd758984bbcf6474b1277365cb5507
Matt Dembiczak <matt@dembiczak.net>
2025-05-30 16:05:02 -0400

    fix output collection task

--- src/fred_pop_gen/task_collect_output_by_county.py
+++ src/fred_pop_gen/task_collect_output_by_county.py
@@ -1,32 +1,21 @@
 from pathlib import Path
 from typing import Annotated
 
-from fred_pop_gen.config import DATA, DATA_CATALOG, STATE_FIPS
 import pandas as pd
 from pytask import DirectoryNode, Product, task
 
+from fred_pop_gen.config import DATA, DATA_CATALOG, STATE_FIPS
 from fred_pop_gen.utils import get_county_fips
 
 
-SCHOOL_ROOT_DIR = DATA / "interim" / "school"
+SCHOOL_ROOT_DIR = DATA / "interim" / f"school_{STATE_FIPS}"
 
 for _county in get_county_fips():
 
     @task(id=_county)
-    def merge_school_output_in_county(
-        pubsch_df: Annotated[
-            pd.DataFrame, DATA_CATALOG[f"persons_w_public_school_{_county}"]
-        ],
-        privsch_df: Annotated[
-            pd.DataFrame, DATA_CATALOG[f"persons_w_private_school_{_county}"]
-        ],
-    ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_school_{_county}"]]:
-        return pd.concat([pubsch_df, privsch_df])
-
-    @task(id=_county)
-    def serialize_school_output_in_county(
+    def task_serialize_school_output_in_county(
         county: Annotated[str, _county],
-        df: Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_school_{_county}"]],
+        df: Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_public_school_{_county}"]],
         dir: Annotated[
             Path,
             DirectoryNode(root_dir=SCHOOL_ROOT_DIR, pattern=_county),
@@ -38,9 +27,17 @@ for _county in get_county_fips():
 
 @task(after="serialize_school_output_in_county")
 def task_collect_school_output(
-    paths: Annotated[list[Path], DirectoryNode(root_dir=SCHOOL_ROOT_DIR, pattern="*")],
+    pubsch_paths: Annotated[
+        list[Path], DirectoryNode(root_dir=SCHOOL_ROOT_DIR, pattern="*")
+    ],
+    privsch_df: Annotated[
+        pd.DataFrame, DATA_CATALOG[f"persons_w_private_school_{STATE_FIPS}"]
+    ],
     p_df: Annotated[pd.DataFrame, DATA_CATALOG[f"persons_{STATE_FIPS}"]],
 ) -> Annotated[pd.DataFrame, DATA_CATALOG[f"persons_w_school_{STATE_FIPS}"]]:
-    sch_df = pd.concat([pd.read_pickle(path) for path in paths])
+    pubsch_df = pd.concat([pd.read_pickle(path) for path in pubsch_paths])
+    sch_df = pd.concat([pubsch_df, privsch_df])
+
     p_df["school_id"] = sch_df["school_id"]
+
     return p_df
